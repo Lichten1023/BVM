fbe76f23285e2db37c43a017385f2f8c4b002393
Blackhill1023 <narasan1023@gmail.com>
2025-07-01 11:15:52 +0900

    v1.25.7.01.1

--- lily/program.py
+++ lily/program.py
@@ -2,7 +2,6 @@ from core import vm
 
 def Run(Program:str) -> list:
     Program = PreprocessAssembly(Program)
-    print(Program)
     Program = AnalyzeAssemblyMetadata(Program)
     ProcessAssembly(Program, Verbose=Verbose)
 
@@ -42,33 +41,43 @@ def AnalyzeAssemblyMetadata(Program:list):  # メタデータの解析
     return Program
 
 def ProcessAssembly(Program:list, Verbose:bool=False) -> None:
+    LineIndexList = []  # 行番号を格納するリスト
     for i in range(len(Program)):
         ProcessedProgram = []   # 処理済みプログラムを格納するリスト
-        if Program[i][0] == "LDI":  # LDI命令を処理
-            ProcessedProgram.append(Program[i][0])
-            ProcessedProgram.append(int(Program[i][1][1]))
-            ProcessedProgram.append(int(Program[i][2]))
-        elif Program[i][0] == "ADD" or Program[i][0] == "SUB" or Program[i][0] == "MUL":    # ADD命令を処理
-            ProcessedProgram.append(Program[i][0])
-            ProcessedProgram.append(int(Program[i][1][1]))
-            ProcessedProgram.append(int(Program[i][2][1]))
-        elif Program[i][0] == "HALT":   # HALT命令を処理
-            ProcessedProgram.append(Program[i][0],)
-        elif Program[i][0] == "JMP":  # JMP命令を処理
-            ProcessedProgram.append(Program[i][0])
-            ProcessedProgram.append(int(Program[i][1]))
-        Program[i] = tuple(ProcessedProgram)
+    
+        LineIndexList.append(int(Program[i][0]))  # 行番号を追加
 
+        if Program[i][1] == "LDI":  # LDI命令を処理
+            ProcessedProgram.append(Program[i][1])
+            ProcessedProgram.append(int(Program[i][2][1]))
+            ProcessedProgram.append(int(Program[i][3]))
+        elif Program[i][1] == "ADD" or Program[i][1] == "SUB" or Program[i][1] == "MUL":    # ADD命令を処理
+            ProcessedProgram.append(Program[i][1])
+            ProcessedProgram.append(int(Program[i][2][1]))
+            ProcessedProgram.append(int(Program[i][3][1]))
+        elif Program[i][1] == "HALT":   # HALT命令を処理
+            ProcessedProgram.append(Program[i][1])
+        elif Program[i][1] == "JMP":  # JMP命令を処理
+            ProcessedProgram.append(Program[i][1])
+            ProcessedProgram.append(int(Program[i][2]))
+        elif Program[i][1] == "NOP":   # NOP命令を処理
+            ProcessedProgram.append(Program[i][1])
+
+        Program[i] = tuple(ProcessedProgram)
+    
     print(Program)  # 処理済みプログラムを表示
+    print(LineIndexList)  # 行番号リストを表示
 
     PC = 0  # プログラムカウンタを初期化
     VM = vm(verbose=Verbose)  # VMのインスタンスを生成して実行
 
     while PC < len(Program):  # プログラムカウンタがプログラムの長さより小さい間ループ
         if Program[PC][0] == "JMP":
-            PC = Program[PC][1] - 1  # JMP命令の処理
+            PC = LineIndexList.index(int(Program[PC][1]))  # JMP命令の処理
+            if Verbose:
+                print(f"Jumping to instruction at address {LineIndexList[PC]}.\n")
         else:
-            VM.RunAssembly(*Program[PC])
+            VM.RunAssembly(LineIndexList[PC], *Program[PC])
             PC += 1 # プログラムカウンタをインクリメント
     pass
 
@@ -76,11 +85,13 @@ program = """
 VERBOSE
 // This is a comment
 
-LDI R1 2    // Load immediate value 2 into register R1
-LDI R2 3    // Load immediate value 3 into register R2
-ADD R1 R2   // Add values in R1 and R2
-SUB R1 R2   // Subtract values in R1 and R2
-HALT       // Stop execution
+10 LDI R1 2    // Load immediate value 2 into register R1
+20 LDI R2 3    // Load immediate value 3 into register R2
+30 NOP         // No operation (padding)
+31 JMP 50 // Jump to instruction at address 50
+40 ADD R1 R2   // Add values in R1 and R2
+50 SUB R1 R2   // Subtract values in R1 and R2
+60 HALT       // Stop execution
 """
     
 Run(program)
